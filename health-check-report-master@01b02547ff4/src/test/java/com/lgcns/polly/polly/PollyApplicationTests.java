c97e2556dbd1d1311a9ee2183488129ddc1e293b
Elliot <elliot@Elliotui-MacBookPro.local>
2024-02-13 14:14:32 +0900

    temp

--- health-check-report-master@01b02547ff4/src/test/java/com/lgcns/polly/polly/PollyApplicationTests.java
+++ health-check-report-master@01b02547ff4/src/test/java/com/lgcns/polly/polly/PollyApplicationTests.java
@@ -21,7 +21,7 @@ class PollyApplicationTests {
 	@DisplayName("수기로 전송할 때 사용")
 	void testPolly() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date date = sdf.parse("2023-08-04");
+		Date date = sdf.parse("2023-12-29");
 
 		controller.sendPollyMessageV2(date, "all");//all
 //		controller.sendPollyMessageV2(date, "elliot");//all
